f79f4725d0fbb3b58b1e464b1e1c23ad48b09486
Nikhil Saxena <nikhil.saxena@sepl.in>
2023-04-25 17:59:38 +0530

    middleware depen download and used

--- src/component/REDUX/index1.js
+++ src/component/REDUX/index1.js
@@ -1,8 +1,17 @@
 // if react then import redux from 'redux'
 // but its a js so
+//middleware:provide a third party extension point between dispatching a action and the momment it reaches the reducer
+//can use for logging crashing report,perfoming asynchoning
+// the middleware here we will be using is :redux-logger"
+// npm install redux-logger
+// we will need logger so will import it :createLogger method
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
+
 const combinedStore = redux.combineReducers;
 const createStore = redux.createStore;
+const logger = reduxLogger.createLogger();
+const applyMiddleWare = redux.applyMiddleware;
 
 const cakeInitialState = { cake: 10 };
 const iceCreamInitialState = { iceCream: 10 };
@@ -51,8 +60,13 @@ const storeCombined = combinedStore({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
 });
-const store = createStore(storeCombined);
+const store = createStore(storeCombined, applyMiddleWare(logger));
+// const unsubscribe = store.subscribe(() => {
+//   console.log("updatedState", store.getState());
+// });
+const unsubscribe = store.subscribe(() => {});
 console.log(store.getState());
 // store.dispatch({ type: "eating" });
 store.dispatch({ type: "eatingCake" });
-console.log(store.getState());
+// console.log(store.getState());
+unsubscribe();
