f62dad8019f5383291f2114472e07205f6078297
coderwith-zeroskills <113875404+coderwith-zeroskills@users.noreply.github.com>
2023-04-25 12:09:50 +0530

    Update First.js

--- src/component/Concept/GitBas/First.js
+++ src/component/Concept/GitBas/First.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function First() {
-  return <div>vvvv</div>;
+  return <div>changes done in github and will be reflect in vscode by pull</div>;
 }
 
 export default First;
